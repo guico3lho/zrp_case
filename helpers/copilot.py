c6cb0e2152333476e524e8c32eb5d001885b32e9
GuiCoelhoDev <guiminervino@gmail.com>
2023-03-07 17:57:33 -0300

    Adiciona dataframes usados no projeto

--- helpers/copilot.py
+++
@@ -1,8 +0,0 @@
-# get the maximum and minimum value of a element of list on a pandas series
-
-# split a series of lists into multiple series
-# https://stackoverflow.com/questions/50670274/split-a-series-of-lists-into-multiple-series
-
-# change types of columns in pandas dataframe
-df = df.ast
-
